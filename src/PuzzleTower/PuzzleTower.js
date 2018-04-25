37a1582e5b156df28e6ba54e53a22d71d140cec7
lingra <lingra@kth.se>
2018-04-25 22:26:12 +0200

    working base

--- src/PuzzleTower/PuzzleTower.js
+++ src/PuzzleTower/PuzzleTower.js
@@ -2,42 +2,57 @@ import React, {Component} from 'react';
 import './PuzzleTower.css';
 import {modelInstance} from '../data/portfolioModel';
 import { Link } from 'react-router-dom';
+import ImgLogo from "../images/escapetower.png";
 
 
 class PuzzleTower extends Component {
 
   render() {
+      var TitleProject = "Puzzle Tower VR";
       
+      var Members = ["Lukas Saari", "Linnéa Granlund", "Nicolas Zimmermann", "Linette Nilsson", "Olivia Bengtsson", "Thibault Laval"];
+      var MemberDiv = Members.map((member) => <li>{member}</li>);
+      
+      var Description = "This is a puzzle VR game that we made in the course 'Computer Game Design' at KTH in my 4th year. The idea behind this is a child-friendly VR game that isn't scary. It's also funny to see someone else play it since you need to interact with the environment to solve the puzzles. /nTo add more VR feeling we made it so that when you have solved a puzzle you fall a short period to the next puzzle.";
+                                  
+      var Weblink = "https://people.kth.se/~oliben/puzzletower/";
+      
+      var Members = ["Lukas Saari", "Linnéa Granlund", "Nicolas Zimmermann", "Linette Nilsson", "Olivia Bengtsson", "Thibault Laval"];
+      var MemberDiv = Members.map((member) => <li>{member}</li>);
       return (
           <div>
-                <div class="row">
-                    <div class="col-sm-2"></div>
-                    <div class="col-sm-4">
-                        <img src="images/escapetower.png" style={"padding:20px;"} width="300px" />
-                    </div>
-                    <div class="col-sm-4">
-
-                        <li>Lukas Saari</li>
-                        <li>Linnéa Granlund</li>
-                        <li>Nicolas Zimmermann</li>
-                        <li>Linette Nilsson</li>
-                        <li>Olivia Bengtsson</li>
-                        <li>Thibault Laval</li>
-
-                    </div>
-                    <div class="col-sm-2"></div>
-                </div>
-
-
-                <div class="row">
-                    <div class="col-sm-12">
-                        <p class="weblink">
-                            <a href="https://people.kth.se/~oliben/puzzletower/">
-                                https://people.kth.se/~oliben/puzzletower/
-                            </a>
-                        </p>
-                    </div>
-                </div>
+              <div class="row">
+                  <div class="col-sm-2"></div>
+                  <div class="col-sm-4">
+                      <img id="imgBig" src={ImgLogo} />
+                  </div>
+                  <div class="col-sm-4">
+                      <h3>{TitleProject}</h3>
+                      {MemberDiv}
+                      
+                  </div>
+                  <div class="col-sm-2"></div>
+              </div>
+              
+              <div class="row">
+                  <div class="col-sm-2"></div>
+                  <div className="col-sm-6" id="descriptionDiv">
+                      <p>{Description}</p>
+                  </div>
+                  <div class="col-sm-4"></div>
+              </div>
+              
+              <div class="row">
+                  <div class="col-sm-12">
+                      <p class="weblink">
+                          <a href={Weblink}>
+                              {Weblink}
+                          </a>
+                      </p>
+                  </div>
+              </div>
+            
+                                      
           </div>
       );
   }
